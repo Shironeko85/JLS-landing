8b03f7ff483c5c8a4d3b4ac71a66a0f7e7e0236c
Javier Vallejos <160947104+Shironeko85@users.noreply.github.com>
2025-11-16 16:57:38 +0000

    Test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@
 const Navbar = () => {
   return (
     <>
-        <div class="bg-rose-950 w-100% h-20 flex items-center">
-            <div class="w-5 h-5 ml-5 rounded-4xl bg-neutral-400 ">
-                <img class="h-5 w-5 -translate-y-1/12" src="/img/Triforce.png"></img>
+        <div className="bg-rose-950 w-100% h-20 flex items-center">
+            <div className="w-5 h-5 ml-5 rounded-4xl bg-neutral-400 ">
+                <img className="h-5 w-5 -translate-y-1/12" src="/img/Triforce.png"></img>
             </div>
-            <div class="text-white text-1xl ml-2">Triforce</div>
+            <div className="text-white text-1xl ml-2">Triforce</div>
         </div>
     </>
   )
