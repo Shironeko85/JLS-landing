101218d2fc5c7b6ef750afae0cf02957055ff4da
Javier Vallejos <160947104+Shironeko85@users.noreply.github.com>
2025-11-22 03:07:10 +0000

    22/11

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ const Navbar = () => {
   return (
     <>
         <div className="bg-rose-950 w-100% h-20 flex items-center">
-            <div className="w-5 h-5 ml-5 rounded-4xl bg-neutral-400 ">
-                <img className="h-5 w-5 -translate-y-1/12" src="/img/Triforce.png"></img>
+            <div className="w-7 h-7 flex justify-center object-center ml-5 rounded-4xl bg-neutral-400 ">
+                <img className="h-6 w-6" src="/img/Triforce.png" />
             </div>
-            <div className="text-white text-1xl ml-2">Triforce</div>
+            <div className="text-white text-1xl ml-2">Triforce Solutions </div>
         </div>
     </>
   )
